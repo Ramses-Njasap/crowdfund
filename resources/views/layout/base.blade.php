128ad7b3bf551387568d631d2d7b694cbf51f7de
Ramses Njasap <56199714+Ramses-Njasap@users.noreply.github.com>
2024-08-23 13:32:49 +0100

    Update base.blade.php

--- resources/views/layout/base.blade.php
+++ resources/views/layout/base.blade.php
@@ -214,7 +214,7 @@
                         <span aria-hidden="true">&times;</span>
                     </button>
                 </div>
-                <form action="{{ route('crowdfund.store') }}" method="POST" enctype="multipart/form-data">
+                <form action="{{ secure_route('crowdfund.store') }}" method="POST" enctype="multipart/form-data">
                     @csrf
                     <div class="modal-body">
                         <div class="form-group">
