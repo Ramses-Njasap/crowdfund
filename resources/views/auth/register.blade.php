6d742c4b29bc0917969ac3b8ecc790ea5056ec04
Ramses Njasap <56199714+Ramses-Njasap@users.noreply.github.com>
2024-08-23 13:36:14 +0100

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
                     <h4 class="mb-0">Register</h4>
                 </div>
                 <div class="card-body">
-                    <form method="POST" action="{{ url('register') }}">
+                    <form method="POST" action="{{ secure_url('register') }}">
                         @csrf
 
                         <!-- Name Input -->
